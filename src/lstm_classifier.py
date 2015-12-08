cfaf4d72d1cb6b64ec714783e578d4fe54ba7883
dirkhovy <mail@dirkhovy.com>
2015-12-08 16:30:07 +0100

    added args for dimensionality, dropout, noise, and stats update frequency. Also, cleaned up the code some more

--- src/lstm_classifier.py
+++ src/lstm_classifier.py
@@ -11,7 +11,13 @@ parser.add_argument('--train', help='train files')
 parser.add_argument('--test', help='test files')
 parser.add_argument('--target', help='predict age, gender, both of them, or the joint cross-product',
                     choices=('age', 'gender', 'both', 'joint'), default='both')
-parser.add_argument('--iterations', help='number of iterations', default=5000)
+parser.add_argument('--iterations', help='number of iterations', default=50)
+parser.add_argument('--status', help='number of processed instances between status updates', default=10000, type=int)
+parser.add_argument('--noise', help='amount of noise added to embeddings', default=0.1, type=float)
+parser.add_argument('--n_embeddings', help='dimensionality of word embeddings', default=128, type=int)
+parser.add_argument('--n_hidden', help='dimensionality of hidden LSTM layer', default=50, type=int)
+parser.add_argument('--n_output', help='dimensionality of output layer', default=32, type=int)
+parser.add_argument('--dropout', help='dropout probability for final sentence representation', default=0.0, type=float)
 args = parser.parse_args()
 
 # read in data
@@ -75,12 +81,27 @@ for sentence, label in train:
     words.update(sentence)
     labels.add(label)
 words.add("_UNK_")
+words.add("_EOS_")
 
 i2w = list(words)
 w2i = {word: i for i, word in enumerate(i2w)}
 i2l = list(labels)
 l2i = {label: i for i, label in enumerate(i2l)}
 UNK = w2i["_UNK_"]
+EOS = w2i["_EOS_"]
+
+# convert words to word_indices
+for i, (sentence, label) in enumerate(train):
+    word_indices = [w2i.get(word, UNK) for word in sentence]
+    word_indices.append(EOS)
+    label_index = l2i[label]
+    train[i] = (word_indices, label_index)
+
+for i, (sentence, label) in enumerate(test):
+    word_indices = [w2i.get(word, UNK) for word in sentence]
+    word_indices.append(EOS)
+    label_index = l2i[label]
+    test[i] = (word_indices, label_index)
 
 num_words = len(w2i)
 num_labels = len(l2i)
@@ -94,17 +115,17 @@ sgd = pycnn.SimpleSGDTrainer(model)
 
 print("declared model and trainer", file=sys.stderr)
 
-WORD_EMBEDDING_SIZE = 128
-LSTM_HIDDEN_LAYER_SIZE = 50
-MLP_HIDDEN_LAYER_SIZE = 32
+WORD_EMBEDDING_SIZE = args.n_embeddings
+LSTM_HIDDEN_LAYER_SIZE = args.n_hidden
+MLP_HIDDEN_LAYER_SIZE = args.n_output
 
 model.add_lookup_parameters("word_lookup", (num_words, WORD_EMBEDDING_SIZE))
 pH = model.add_parameters("HID", (MLP_HIDDEN_LAYER_SIZE, LSTM_HIDDEN_LAYER_SIZE))
 pO = model.add_parameters("OUT", (num_labels, MLP_HIDDEN_LAYER_SIZE))
 print("declared variables", file=sys.stderr)
 
-# builder = pycnn.LSTMBuilder(1, WORD_EMBEDDING_SIZE, LSTM_HIDDEN_LAYER_SIZE, model)
-builder = pycnn.SimpleRNNBuilder(1, WORD_EMBEDDING_SIZE, LSTM_HIDDEN_LAYER_SIZE, model)
+builder = pycnn.LSTMBuilder(1, WORD_EMBEDDING_SIZE, LSTM_HIDDEN_LAYER_SIZE, model)
+# builder = pycnn.SimpleRNNBuilder(1, WORD_EMBEDDING_SIZE, LSTM_HIDDEN_LAYER_SIZE, model)
 print("declared builder", file=sys.stderr)
 
 
@@ -122,7 +143,7 @@ def build_tagging_graph(word_indices, model, builder):
 
     # retrieve embeddings from the model and add noise
     word_embeddings = [pycnn.lookup(model["word_lookup"], w) for w in word_indices]
-    word_embeddings = [pycnn.noise(we, 0.1) for we in word_embeddings]
+    word_embeddings = [pycnn.noise(we, args.noise) for we in word_embeddings]
 
     # compute the expressions for the forward pass
     forward_sequence = [x.output() for x in f_init.add_inputs(word_embeddings)]
@@ -144,6 +165,7 @@ def fit(word_indices, label, model, builder):
 
     # retrieve model parameters
     final_state = forward_states[-1]
+    final_state = pycnn.dropout(final_state, 0.1)
     # print("final state", final_state, file=sys.stderr)
     H = pycnn.parameter(pH)
     O = pycnn.parameter(pO)
@@ -151,8 +173,6 @@ def fit(word_indices, label, model, builder):
     # print(pycnn.cg().PrintGraphviz())
 
     # TODO: add bias terms
-    # print("multiplying stuff!", file=sys.stderr)
-
     r_t = O * (pycnn.tanh(H * final_state))
 
     return pycnn.pickneglogsoftmax(r_t, label)
@@ -179,51 +199,60 @@ def predict(word_indices, model, builder):
     out = pycnn.softmax(r_t)
     chosen = np.argmax(out.npvalue())
 
-    return i2l[chosen]
+    return chosen
 
 
-total_loss = 0
+def evaluate(data_set, model, builder):
+    """
+    evaluate a test file
+    :param data_set: the converted input file, i.e., a list of ([word_indices], label_index)
+    :param model: current model
+    :param builder: current builder
+    :return: per-token accuracy
+    """
+    good = 0.0
+    bad = 0.0
+
+    for test_sentence, test_label in data_set:
+        predicted_label = predict(test_sentence, model, builder)
+
+        if predicted_label == test_label:
+            good += 1
+        else:
+            bad += 1
+
+    return good / (good + bad)
+
+
 num_instances = 1
+losses = []
 
 for iteration in range(args.iterations):
     random.shuffle(train)
 
-    if iteration > 0 and iteration % 10 == 0:
-        print('\nITERATION %s' % (iteration+1), file=sys.stderr)
-
     for i, (sentence, label) in enumerate(train, 1):
-        if num_instances % 10000 == 0:
+        if num_instances % args.status == 0:
+            print('\nITERATION %s, instance %s/%s' % (iteration+1, i, len(train)), file=sys.stderr)
             sgd.status()
-            print('Avg. loss', total_loss / num_instances, file=sys.stderr)
-            total_loss = 0
-            num_instances = 0
+            print('Avg. loss', sum(losses) / len(losses), file=sys.stderr)
+            losses.clear()
 
-        if num_instances % 50000 == 0:
-            good = 0.0
-            bad = 0.0
-
-            for test_sentence, test_label in test:
-                test_word_indices = [w2i.get(word, UNK) for word in test_sentence]
-                predicted_label = predict(test_word_indices, model, builder)
-
-                if predicted_label == test_label:
-                    good += 1
-                else:
-                    bad += 1
-
-            print('='*50, file=sys.stderr)
-            print("Accuracy on test: %s\n" % (good / (good + bad)), file=sys.stderr)
+        if num_instances % (args.status * 5) == 0:
+            print('-'*50, file=sys.stderr)
+            print("Accuracy on test: %s\n" % (evaluate(test, model, builder)), file=sys.stderr)
 
         # TRAINING
-        # convert words to word_indices
-        word_indices = [w2i.get(word, UNK) for word in sentence]
-        label_index = l2i[label]
-
         # fit the shit!
-        error = fit(word_indices, label_index, model, builder)
+        error = fit(sentence, label, model, builder)
 
-        total_loss += error.scalar_value()
-        # print(error.scalar_value(), total_loss)
+        losses.append(error.scalar_value())
         num_instances += 1
         error.backward()
         sgd.update()
+
+    print('='*50, file=sys.stderr)
+    print("Accuracy on test: %s" % (evaluate(test, model, builder)), file=sys.stderr)
+    print('Avg. loss', sum(losses) / len(losses), file=sys.stderr)
+    print('='*50, file=sys.stderr)
+    num_instances = 1
+    losses.clear()
